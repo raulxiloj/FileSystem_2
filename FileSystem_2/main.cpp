6138f682145776a2d1dc1514525066cedf71a329
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-09-04 23:08:13 -0600

    Declaracion de structs para sistema de archivos

--- FileSystem_2/main.cpp
+++ FileSystem_2/main.cpp
@@ -158,9 +158,62 @@ typedef struct{
     char part_name[16]; //Nombre de la particion
 }EBR;
 
-ListaMount *lista = new ListaMount();
+/*
+ * Structs para sistema de archivos
+*/
 
-bool flag_global = true;
+typedef struct{
+    int s_filesystem_type; //Guarda el numero que identifica al sistea de archivos utilizados
+    int s_inodes_count; //Guarda el numero total de inodos
+    int s_blocks_count; //Guarda el numero total de bloques
+    int s_free_blocks_count; //Contiene el numero de bloques libres
+    int s_free_inodes_count; //Contiene el numero inodos libres
+    time_t s_mtime; //Ultima fecha en el que el sistema fue montado
+    time_t s_umtime; //Ultima fecha en que el sistema fue desmontado
+    int s_mnt_count; //Indica cuantas veces se ha montado el sistema
+    int s_magic; //Valor que identifica al sistema de archivos 0xEF53
+    int s_inode_size; //Tamano del inodo
+    int s_block_size; //Tamano del bloque
+    int s_first_ino; //Primer inodo libre
+    int s_first_blo; //Primero bloque libre
+    int s_bm_inode_start; //Guardara el inicio del bitmap de inodos
+    int s_bm_block_start; //Guardara el inicio del bitmap de bloques
+    int s_inode_start; //Guarada el inicio de la tabla de inodos
+    int s_block_start; //Guardara el inicio de la tabla de bloques
+}SuperBloque;
+
+typedef struct{
+    int i_uid; //UID del usuario propiertario del archivo/carpeta
+    int i_gid; //GID del grupo al que pertenece el archivo/carpeta
+    int i_size; //Tamano del archivo en bytes
+    int i_atime; //Ultima fecha en que se leyo el inodo sin modificarlo
+    int i_ctime; //Fecha en que se creo el el inodo
+    int i_mtime; //Ultima fecha en la que se modifco
+    int i_block[15]; //Array de bloques
+    int i_type; //Indica si es archivo o carpeta
+    int i_perm; //Guarada los permisos del archivo/carpeta
+}InodoTable;
+
+typedef struct{
+    char b_name[12];//Nombre carpeta/archivo
+    int b_inodo;//Apuntador hacia un inodo asociado al archivo o carpeta
+}Content;
+
+typedef struct{
+    Content b_content[4];//Array con el contenido de la carpeta
+}BloqueCarpeta;
+
+typedef struct{
+    char b_content[64];
+}BloqueArchivo;
+
+typedef struct{
+    int b_pointer[16];//Array con los apuntadores hacia bloques
+}BloqueApuntadores;
+
+/*Variables globales*/
+ListaMount *lista = new ListaMount();
+bool static flag_global = true;
 
 /*
     FUNCION PRINCIPAL
