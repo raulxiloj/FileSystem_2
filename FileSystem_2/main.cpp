e6d7ed64f13f91a436b23c4831fd98ba836118ad
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2019-09-06 12:34:42 -0600

    Formateo EXT3 listo

--- FileSystem_2/main.cpp
+++ FileSystem_2/main.cpp
@@ -214,6 +214,16 @@ typedef struct{
     int b_pointer[16];//Array con los apuntadores hacia bloques
 }BloqueApuntadores;
 
+typedef struct{
+    int journal_operation_type;
+    int journal_type;//Archivo/Carpeta
+    char journal_name[50];
+    char journal_content[100];
+    time_t journal_date;
+    char journal_owner[20];
+    int journal_permissions;
+}Journal;
+
 /*Variables globales*/
 ListaMount *lista = new ListaMount();
 bool flag_global = true;
@@ -3071,5 +3081,105 @@ void formatearEXT2(int inicio, int tamano, QString direccion){
 }
 
 void formatearEXT3(int inicio, int tamano, QString direccion){
+    double n = (tamano - static_cast<int>(sizeof(SuperBloque)))/(4 + static_cast<int>(sizeof(InodoTable)) +3*static_cast<int>(sizeof(BloqueArchivo)));
+    int num_estructuras = static_cast<int>(floor(n));//Bitmap indos
+    int num_bloques = 3*num_estructuras;//Bitmap bloques
+    int super_size = static_cast<int>(sizeof(SuperBloque));
+    int journal_size = static_cast<int>(sizeof(Journal))*num_estructuras;
 
+    SuperBloque super;
+    super.s_filesystem_type = 3;
+    super.s_inodes_count = num_estructuras;
+    super.s_blocks_count = num_bloques;
+    super.s_free_blocks_count = num_bloques - 2;//---
+    super.s_free_inodes_count = num_estructuras - 2;//---
+    super.s_mtime = time(nullptr);
+    super.s_umtime = 0;
+    super.s_mnt_count = 0;
+    super.s_magic = 0xEF53;
+    super.s_inode_size = sizeof(InodoTable);
+    super.s_block_size = sizeof(BloqueArchivo);
+    super.s_first_ino = 2;
+    super.s_first_blo = 2;
+    super.s_bm_inode_start = inicio + super_size + journal_size;
+    super.s_bm_block_start = inicio + super_size + journal_size + num_estructuras;
+    super.s_inode_start = inicio + super_size + journal_size + num_estructuras + num_bloques;
+    super.s_block_start = inicio + super_size + journal_size + num_estructuras + num_bloques + static_cast<int>(sizeof(InodoTable))*num_estructuras;
+
+    InodoTable inodo;
+    BloqueCarpeta bloque;
+
+    char buffer = '0';
+    char buffer2 = '1';
+
+    FILE *fp = fopen(direccion.toStdString().c_str(),"rb+");
+    /*-------------SUPERBLOQUE----------------*/
+    fseek(fp,inicio,SEEK_SET);
+    fwrite(&super,sizeof(SuperBloque),1,fp);
+    /*-----------BITMAP DE INODOS-------------*/
+    for(int i = 0; i < num_estructuras; i++){
+        fseek(fp,super.s_bm_inode_start + i,SEEK_SET);
+        fwrite(&buffer,sizeof(char),1,fp);
+    }
+    /*---------inodos para / y users.txt------*/
+    fseek(fp,super.s_bm_inode_start,SEEK_SET);
+    fwrite(&buffer2,sizeof(char),1,fp);
+    fwrite(&buffer2,sizeof(char),1,fp);
+    /*----------BITMAP DE BLOQUES-------------*/
+    for(int i = 0; i < num_bloques; i++){
+        fseek(fp,super.s_bm_block_start + i,SEEK_SET);
+        fwrite(&buffer,sizeof(char),1,fp);
+    }
+    /*--------inodo para carpeta root---------*/
+    inodo.i_uid = 1;
+    inodo.i_gid = 1;
+    inodo.i_size = 0;
+    inodo.i_atime = time(nullptr);
+    inodo.i_ctime = time(nullptr);
+    inodo.i_mtime = time(nullptr);
+    inodo.i_block[0] = 0;
+    for(int i = 1; i < 15;i++){
+        inodo.i_block[i] = -1;
+    }
+    inodo.i_type = '0';
+    inodo.i_perm = 664;
+    fseek(fp,super.s_inode_start,SEEK_SET);
+    fwrite(&inodo,sizeof(InodoTable),1,fp);
+    /*-------Bloque para carpeta root-------*/
+    strcpy(bloque.b_content[0].b_name,".");//Padre
+    bloque.b_content[0].b_inodo=0;
+
+    strcpy(bloque.b_content[1].b_name,"..");//Actual
+    bloque.b_content[1].b_inodo=0;
+
+    strcpy(bloque.b_content[2].b_name,"users.txt");
+    bloque.b_content[2].b_inodo=1;
+
+    strcpy(bloque.b_content[3].b_name,".");
+    bloque.b_content[3].b_inodo=-1;
+    fseek(fp,super.s_block_start,SEEK_SET);
+    fwrite(&bloque,sizeof(BloqueCarpeta),1,fp);
+    /*--------inodo para users.txt----------*/
+    inodo.i_uid = 1;
+    inodo.i_gid = 1;
+    inodo.i_size = 27;
+    inodo.i_atime = time(nullptr);
+    inodo.i_ctime = time(nullptr);
+    inodo.i_mtime = time(nullptr);
+    inodo.i_block[0] = 1;
+    for(int i = 1; i < 15;i++){
+        inodo.i_block[i] = -1;
+    }
+    inodo.i_type = '1';
+    inodo.i_perm = 755;
+    fseek(fp,super.s_inode_start + static_cast<int>(sizeof(InodoTable)),SEEK_SET);
+    fwrite(&inodo,sizeof(InodoTable),1,fp);
+    /*--------Bloque para users.txt---------*/
+    BloqueArchivo archivo;
+    memset(archivo.b_content,0,sizeof(archivo.b_content));
+    strcpy(archivo.b_content,"1,G,root\n1,U,root,root,123\n");
+    fseek(fp,super.s_bm_block_start + static_cast<int>(sizeof(BloqueCarpeta)),SEEK_SET);
+    fwrite(&archivo,sizeof(BloqueArchivo),1,fp);
+
+    fclose(fp);
 }
